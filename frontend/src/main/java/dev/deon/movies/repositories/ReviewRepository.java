3053779570e3897c5ebc97963477964d5e80a6a7
Deon Hill <Dehill_12@icloud.com>
2023-10-28 10:42:57 -0400

    Add frontend code

---
+++ frontend/src/main/java/dev/deon/movies/repositories/ReviewRepository.java
@@ -0,0 +1,8 @@
+package dev.deon.movies.repositories;
+
+import dev.deon.movies.models.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
