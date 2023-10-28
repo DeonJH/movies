c8b90250c4c1aa5f0cad7a14db031cc56af049e1
Deon Hill <Dehill_12@icloud.com>
2023-10-28 14:14:25 -0400

    Connect frontend to backend

--- frontend/movie-gold-v1/src/api/axiosConfig.js
+++ frontend/movie-gold-v1/src/api/axiosConfig.js
@@ -1,6 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
+import { API_URL } from './envConfig';
 
 export default axios.create({
-    baseURL:'https://9c96-103-106-239-104.ap.ngrok.io/',
+    baseURL: API_URL,
     headers: {"ngrok-skip-browser-warnings": "true"}
 });
