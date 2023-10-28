ec10009faf410c47ed3866442e3b63b8ceb2e25c
Deon Hill <Dehill_12@icloud.com>
2023-10-28 14:13:33 -0400

    Add front end connections

--- src/main/java/dev/deon/movies/controllers/ReviewController.java
+++ src/main/java/dev/deon/movies/controllers/ReviewController.java
@@ -5,15 +5,13 @@ import dev.deon.movies.models.Review;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/review")
+@CrossOrigin(origins = "${env.FRONTEND_URL}")
 public class ReviewController {
 
     @Autowired
