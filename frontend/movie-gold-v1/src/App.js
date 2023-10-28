843a96fcd9411f90e6ee71decf61709cd2e864d3
Deon Hill <Dehill_12@icloud.com>
2023-10-28 18:37:23 -0400

    Add reviews functionality

--- frontend/movie-gold-v1/src/App.js
+++ frontend/movie-gold-v1/src/App.js
@@ -6,26 +6,48 @@ import {Routes, Route} from 'react-router-dom';
 import Home from './components/home/Home';
 import Header from './components/header/Header';
 import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
 
 function App() {
 
   const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
 
   const getMovies = async () =>{
-
-    try{
+    
+    try
+    {
 
       const response = await api.get("/api/v1/movies");
 
-      console.log(response.data);
-
       setMovies(response.data);
-      
-    } catch(err)
+
+    } 
+    catch(err)
     {
       console.log(err);
     }
+  }
 
+  const getMovieData = async (movieId) => {
+     
+    try 
+    {
+        const response = await api.get(`/api/v1/movies/${movieId}`);
+
+        const singleMovie = response.data;
+
+        setMovie(singleMovie);
+
+        setReviews(singleMovie.reviews);
+        
+
+    } 
+    catch (error) 
+    {
+      console.error(error);
+    }
 
   }
 
@@ -38,8 +60,9 @@ function App() {
       <Header/>
       <Routes>
           <Route path="/" element={<Layout/>}>
-            <Route path="/" element={<Home movies= {movies}/>}></Route>
+            <Route path="/" element={<Home movies={movies} />} ></Route>
             <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+            <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
 
           </Route>
       </Routes>
